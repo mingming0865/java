c94d074f276a9e6ec4f46d6fa156d4c1f276ca1f
mingming0865 <mingming0865@gmail.com>
2025-03-24 08:21:15 +0700

    init project:

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package man;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and Man");
+		System.out.println("Hello world with java and Man");
+		System.out.println("Hello world with java and Man");
+		System.out.println("Hello world with java and Man");
+
+	}
+}
